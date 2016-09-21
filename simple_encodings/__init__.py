e0d2a93c4d539ee496f8c14feae6d9e5dd3539cb
David96 <davidlepplaweber@aospa.co>
2016-09-21 12:44:53 +0200

    Add lots of stuff
    - add bin support with character substitution (lulu code)
    - add z compressed base64 encoded messages
    - add 1337
    - Unicode support

--- simple_encodings/__init__.py
+++ simple_encodings/__init__.py
@@ -2,55 +2,59 @@ from plugin import BasePlugin
 from poezio.xhtml import clean_text
 from poezio import common
 from poezio import tabs
-from poezio import xhtml
 from poezio.config import config
 
 from collections import namedtuple
+from collections import Counter
 
+import base64
 import codecs
 import random
 import re
+import zlib
 
 from simple_encodings import mono
 
 class Plugin(BasePlugin):
+    dicts = {}
+    german_frequency = { 'a': 6.516,
+                         'b': 1.886,
+                         'c': 2.732,
+                         'd': 5.076,
+                         'e': 16.396,
+                         'f': 1.656,
+                         'g': 3.009,
+                         'h': 4.577,
+                         'i': 6.550,
+                         'j': 0.268,
+                         'k': 1.417,
+                         'l': 3.437,
+                         'm': 2.534,
+                         'n': 9.776,
+                         'o': 2.594,
+                         'p': 0.670,
+                         'q': 0.018,
+                         'r': 7.003,
+                         's': 7.270,
+                         't': 6.154,
+                         'u': 4.166,
+                         'v': 0.846,
+                         'w': 1.921,
+                         'x': 0.034,
+                         'y': 0.039,
+                         'z': 1.134,
+                         'ä': 0.578,
+                         'ü': 0.995,
+                         'ö': 0.443,
+                         'ß': 0.307 }
+
     def init(self):
         self.key = config.get("key").encode('utf-8')
         length = 32 - (len(self.key) % 32)
         self.key += bytes([length]) * length
 
-        self.dicts = {}
-        self.german_frequency = { 'a': 6.516,
-                                  'b': 1.886,
-                                  'c': 2.732,
-                                  'd': 5.076,
-                                  'e': 16.396,
-                                  'f': 1.656,
-                                  'g': 3.009,
-                                  'h': 4.577,
-                                  'i': 6.550,
-                                  'j': 0.268,
-                                  'k': 1.417,
-                                  'l': 3.437,
-                                  'm': 2.534,
-                                  'n': 9.776,
-                                  'o': 2.594,
-                                  'p': 0.670,
-                                  'q': 0.018,
-                                  'r': 7.003,
-                                  's': 7.270,
-                                  't': 6.154,
-                                  'u': 4.166,
-                                  'v': 0.846,
-                                  'w': 1.921,
-                                  'x': 0.034,
-                                  'y': 0.039,
-                                  'z': 1.134,
-                                  'ä': 0.578,
-                                  'ü': 0.995,
-                                  'ö': 0.443,
-                                  'ß': 0.307 }
 
+        # TODO: add real help/usage information to the commands…
         self.api.add_command("rot", self.command_rot, "rot n encodes the given message")
         self.api.add_command("bin", self.command_bin, "bin")
         self.api.add_command("hex", self.command_hex, "hex")
@@ -67,15 +71,21 @@ class Plugin(BasePlugin):
         self.api.add_command("crack", self.command_crack, usage="<message>",
                     help="binary decode", short='binary', completion=self.message_completion)
 
-        self.api.add_event_handler("muc_msg", self.muc_message)
+        # insert event handler at the end (-1) so we're always behind the lima-gold one
+        self.api.add_event_handler("muc_msg", self.muc_message, position=-1)
 
+        # preload german dict
         self.get_dict("de")
 
     strip_regex = re.compile(r"[:\s-]")
     bits_regex = re.compile(r"^[01\s]+$")
+    lulu_regex = re.compile(r"^[lu\s]+$")
     hex_regex = re.compile(r"^[0-9a-fA-F]+$")
+    b64_regex = re.compile(r"^[a-zA-Z0-9+/]+={0,2}$")
+    # lima-gold inserts >>[$#] for gold/stealth messages at the beginning
+    gold_regex = re.compile(r"[#$]>>\s")
     def muc_message(self, msg, tab):
-        body = text = xhtml.get_body_from_message_stanza(msg, key=self.key)
+        body = text = self.gold_regex.sub("", msg['body'])
 
         if text:
             (text, decoding) = self.crack(text)
@@ -97,15 +107,32 @@ class Plugin(BasePlugin):
         self.core.send_message(self.hex(msg))
 
     def command_enc(self, msg):
-        encodings = msg.split(' ')[0]
+        args = msg.split(' ')
+        encodings = args[0]
         msg = msg[len(encodings) + 1:]
 
         if 'r' in encodings:
             msg = self.rotn(msg, random.randrange(1, 26))
-        if 'b' in encodings:
+
+        if '3' in encodings:
+            msg = msg.translate(str.maketrans(
+                'ABCDEFGHIJKLMNOPQRSTUVQXYZabcdefghijklmnopqrstuvwxyz',
+                '48(D3FGH1JKLMN0PQR57UVWXYZ48(d3fgh1jklmn0pqr57uvwxyz'))
+
+        if 'l' in encodings:
+            letters = args[1]
+            if len(letters) == 2:
+                msg = self.bin(msg[3:])
+                msg = msg.replace('1', letters[1]).replace('0', letters[0])
+            else:
+                self.api.information("Lulu code needs letters as 2nd arg")
+        elif 'b' in encodings:
             msg = self.bin(msg)
+
         if 'h' in encodings:
             msg = self.hex(msg)
+        if 'z' in encodings:
+            msg = self.z_encode(msg)
 
         self.core.send_message(msg)
 
@@ -141,25 +168,62 @@ class Plugin(BasePlugin):
         self.api.information("%s%s" % (decoding, text))
 
     def crack(self, text):
-        stripped = self.strip_regex.sub("", text.strip('" '))
         decoding = ""
 
-        is_bin = lambda x: self.bits_regex.match(x) is not None
-        is_hex = lambda x: self.hex_regex.match(x) is not None
-        while is_bin(stripped) or is_hex(stripped):
-            if is_bin(stripped):
-                text = self.bin_decode(stripped)
-                decoding = "%sbinary: " % decoding
+        if self.is_german(text):
+            return (text, "")
+
+        if self.b64_regex.match(text) and len(text) % 4 == 0:
+            try:
+                text = self.z_decode(text)
+                decoding = "%sz: " % decoding
+            except:
+                pass
+
+        stripped = self.strip_regex.sub("", text.strip('" '))
+        counter = Counter(text)
+        is_bin = lambda x: self.bits_regex.match(x) is not None and len(x) % 8 == 0
+        is_hex = lambda x: self.hex_regex.match(x) is not None and len(x) % 2 == 0
+        is_lulu = lambda x: len(set(stripped)) == 2 and len(stripped) % 8 == 0
+        is_1337 = lambda c, l: c['0'] + c['1'] + c['3'] + c['4'] + c['5'] + c['7'] + c['8'] + c['('] > l / 5
+        is_text = lambda x: sum([1 for c in x if ord(c) >= 32]) >= len(x) / 2
+        while is_bin(stripped) or is_hex(stripped) or is_lulu(stripped) or is_1337(counter, len(text)):
+            #if is_bin(stripped):
+            #    text = self.bin_decode(stripped)
+            #    decoding += "binary: "
+            if is_1337(counter, len(text)):
+                text = text.translate(str.maketrans(
+                    '48(D3FGH1JKLMN0PQR57UVWXYZ48(d3fgh1jklmn0pqr57uvwxyz',
+                    'ABCDEFGHIJKLMNOPQRSTUVQXYZabcdefghijklmnopqrstuvwxyz'))
+                counter = Counter(text)
+                decoding += "1337: "
+            elif is_lulu(stripped):
+                letters = list(set(stripped))
+                try:
+                    guess = self.bin_decode(stripped.translate(str.maketrans("".join(letters), "10")))
+                    decoding += "bin(%s): " % (letters[0] + letters[1])
+                except:
+                    guess = None
+                if guess is not None and len(guess) > 0 and is_text(guess):
+                    text = guess
+                else:
+                    try:
+                        text = self.bin_decode(stripped.translate(str.maketrans("".join(letters), "01")))
+                        decoding += "bin(%s): " % (letters[1] + letters[0])
+                    except:
+                        pass
             elif is_hex(stripped):
                 text = self.hex_decode(stripped)
-                decoding = "%shex: " % decoding
+                decoding += "hex: "
+            if not text:
+                break
             stripped = self.strip_regex.sub("", text)
 
-        if len(text) > 0 and not self.is_german(text):
+        if text is not None and len(text) > 0 and not self.is_german(text):
             res = self.rot_crack(text, True)
             if res:
                 text = res.text
-                decoding = "%srot(%d): " % (decoding, res.n)
+                decoding += "rot(%d): " % res.n
         return (text, decoding)
 
     def rot_crack(self, msg, only_exact=False):
@@ -178,12 +242,12 @@ class Plugin(BasePlugin):
 
     def bin_decode(self, text):
         if len(text) % 8 == 0:
-            return "".join([ chr(c) for c in [ int(text[i:i + 8], 2) for i in range(0, len(text), 8) ] ])
+            return self.get_bytes("".join([ chr(c) for c in [ int(text[i:i + 8], 2) for i in range(0, len(text), 8) ] ])).decode("utf-8")
         return None
 
     def hex_decode(self, text):
         if len(text) % 2 == 0:
-            return "".join([ chr(c) for c in [ int(text[i:i + 2], 16) for i in range(0, len(text), 2) ] ])
+            return self.get_bytes("".join([ chr(c) for c in [ int(text[i:i + 2], 16) for i in range(0, len(text), 2) ] ])).decode("utf-8")
         return None
 
     def is_german(self, msg):
@@ -224,6 +288,9 @@ class Plugin(BasePlugin):
         self.dicts[lang] = self._get_dict(lang)
         return self.dicts[lang]
 
+    def get_bytes(self, s):
+        return bytes([ ord(x) for x in s ])
+
     def rotn(self, msg, n):
         res = ""
         for c in msg:
@@ -236,11 +303,18 @@ class Plugin(BasePlugin):
         return res
 
     def bin(self, msg):
-        return "".join([ str(bin(ord(b))[2:]).zfill(8) for b in msg])
+        return "".join([ str(bin(b)[2:]).zfill(8) for b in msg.encode("utf-8")])
 
     def hex(self, msg):
-        return "".join([ str(hex(ord(b))[2:]).zfill(2) for b in msg])
+        return "".join([ str(hex(b)[2:]).zfill(2) for b in msg.encode("utf-8")])
 
+    def z_encode(self, msg):
+        compressed = zlib.compress(msg.encode('utf-8'), 9)
+        return base64.b64encode(compressed).decode('utf-8')
+
+    def z_decode(self, msg):
+        gz = base64.b64decode(msg)
+        return zlib.decompress(gz).decode('utf-8')
 
     def message_completion(self, the_input):
         def find_message(txt):
